e4a285621ec4535779652670056446cf1bc70e94
Hiroshi Saito <h_saito@colopl.co.jp>
2019-01-30 22:24:32 +0900

    [quiz13_category_widget] 解答

--- task_02_category_widget/lib/category.dart
+++ task_02_category_widget/lib/category.dart
@@ -6,17 +6,27 @@
 // https://www.dartlang.org/guides/language/effective-dart/style#ordering
 import 'package:flutter/material.dart';
 
+final _rowHeight = 100.0;
+final _borderRadius = BorderRadius.circular(_rowHeight / 2);
+
 /// A custom [Category] widget.
 ///
 /// The widget is composed on an [Icon] and [Text]. Tapping on the widget shows
 /// a colored [InkWell] animation.
 class Category extends StatelessWidget {
+  final String name;
+  final IconData icon;
+  final ColorSwatch color;
+
   /// Creates a [Category].
   ///
   /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
   /// the UI, and the icon that represents it (e.g. a ruler).
-  // TODO: You'll need the name, color, and iconLocation from main.dart
-  const Category();
+  const Category(
+      {@required this.name, @required this.icon, @required this.color})
+      : assert(name != null),
+        assert(color != null),
+        assert(icon != null);
 
   /// Builds a custom widget that shows [Category] information.
   ///
@@ -27,7 +37,30 @@ class Category extends StatelessWidget {
   // Theme ancestor in the tree. Below, we obtain the display1 text theme.
   // See https://docs.flutter.io/flutter/material/Theme-class.html
   Widget build(BuildContext context) {
-    // TODO: Build the custom widget here, referring to the Specs.
-    return Container();
+    return Material(
+        child: Container(
+            height: _rowHeight,
+            padding: EdgeInsets.all(8.0),
+            child: InkWell(
+                borderRadius: _borderRadius,
+                highlightColor: color,
+                onTap: () {
+                  print('I was tapped!');
+                },
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Icon(
+                        Icons.cake,
+                        size: 60.0,
+                      ),
+                    ),
+                    Text(
+                      this.name,
+                      style: Theme.of(context).textTheme.headline,
+                    ),
+                  ],
+                ))));
   }
 }
