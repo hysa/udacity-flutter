fd0853c701d16b620fe3ef78ddc69ce34272d3cb
Hiroshi Saito <h_saito@colopl.co.jp>
2019-02-03 22:42:12 +0900

    [task_04_navigation] 回答

--- task_04_navigation/lib/converter_route.dart
+++ task_04_navigation/lib/converter_route.dart
@@ -16,25 +16,30 @@ import 'package:task_04_navigation/unit.dart';
 class ConverterRoute extends StatelessWidget {
   /// Units for this [Category].
   final List<Unit> units;
+  final name;
+  final color;
 
   /// This [ConverterRoute] requires the name, color, and units to not be null.
-  // TODO: Pass in the [Category]'s name and color
   const ConverterRoute({
     @required this.units,
-  }) : assert(units != null);
+    @required this.name,
+    @required this.color,
+  })  : assert(units != null),
+        assert(name != null),
+        assert(color != null);
 
   @override
   Widget build(BuildContext context) {
     // Here is just a placeholder for a list of mock units
     final unitWidgets = units.map((Unit unit) {
-      // TODO: Set the color for this Container
       return Container(
         margin: EdgeInsets.all(8.0),
         padding: EdgeInsets.all(16.0),
+        color: color,
         child: Column(
           children: <Widget>[
             Text(
-              unit.name,
+              name,
               style: Theme.of(context).textTheme.headline,
             ),
             Text(
